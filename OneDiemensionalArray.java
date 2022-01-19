f30bfbd373f69c3a93099da59d65323da4576294
Nadeem531 <97834155+Nadeem531@users.noreply.github.com>
2022-01-18 23:27:31 -0800

    Add files via upload

---
+++ OneDiemensionalArray.java
@@ -0,0 +1,17 @@
+package array_examples;
+
+public class OneDiemensionalArray {
+
+	public static void main(String args[])
+	{
+		int[] a; 
+		a = new int[3]; 
+		for (int i = 0; i < 3; i++) {
+			a[i] = 100;
+			System.out.println(a[i]);
+		}
+	}
+}
+
+
+
